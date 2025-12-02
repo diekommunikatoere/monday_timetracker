4c83dcd19b1dadc2de006d450c3bd8aaf61ed024
Steven Sullivan <ssullivan@diekommunikatoere.de>
2025-12-02 09:35:39 +0100

    refactor(time-entry): migrate ManualTimeEntryButton add icon to centralized Icon

--- components/ManualTimeEntryButton.tsx
+++ components/ManualTimeEntryButton.tsx
@@ -1,9 +1,9 @@
 import { Button } from "@mantine/core";
-import Add from "@/components/icons/Add";
+import { Icon } from "@/components/Icon";
 
 export default function ManualTimeEntryButton(props: { onClick: () => void }) {
 	return (
-		<Button onClick={props.onClick} aria-label="Manuelle Zeiteingabe" aria-controls="manualTimeEntryModal" leftSection={<Add fillColor="#FFFFFF" />}>
+		<Button onClick={props.onClick} aria-label="Manuelle Zeiteingabe" aria-controls="manualTimeEntryModal" leftSection={<Icon name="add" color="#FFFFFF" />}>
 			Zeit eintragen
 		</Button>
 	);
