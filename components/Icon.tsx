9fca2fb972176841424b18291f8925cd2d08c31f
Steven Sullivan <ssullivan@diekommunikatoere.de>
2026-01-27 10:19:22 +0100

    refactor(ui): update icon import path

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,5 +1,5 @@
 import { IconProps } from "@/types/IconProps";
-import * as icons from "./icons";
+import * as icons from "./ui/icons";
 
 export function Icon({ name, size = 24, color = "#282616", className }: IconProps) {
 	const IconComponent = icons[name as keyof typeof icons] as React.ComponentType<{ size?: number; color?: string }>;
