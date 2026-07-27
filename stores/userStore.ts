f555d5a52569a47a50a2c0b0b7ac9ba5a7dda838
Steven Sullivan <ssullivan@diekommunikatoere.de>
2026-07-27 09:08:02 +0200

    refactor(userStore): Remove unused team_ids property from MondayUser type

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -34,7 +34,6 @@ type MondayUser = {
 		thumb_small: string | null;
 		tiny: string | null;
 	} | null;
-	team_ids?: string[] | null;
 	isAdmin: boolean;
 	isGuest: boolean;
 	isViewOnly: boolean;
@@ -125,7 +124,6 @@ export const useUserStore = create<UserState>()(
 				const user = {
 					id: useUserStore.getState().mondayUser!.id,
 					email: useUserStore.getState().mondayUser!.email || "",
-					team_ids: useUserStore.getState().mondayUser!.team_ids || [],
 					is_admin: useUserStore.getState().mondayUser!.isAdmin || false,
 				};
 				set({ supabaseUser: user });
